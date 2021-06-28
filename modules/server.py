8bb5bb03a8219f562db7096ed6fb75ac06771cfb
ayush aggarwal <ayushaggar@gmail.com>
2021-06-28 12:46:33 +0530

    server script

---
+++ modules/server.py
@@ -0,0 +1,124 @@
+import glob
+import string
+import unicodedata
+from bottle import route, run
+import sys
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax()
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return Variable(torch.zeros(1, self.hidden_size))
+
+
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters)
+
+
+def findFiles(path): return glob.glob(path)
+
+# Turn a Unicode string to plain ASCII, thanks to
+# http://stackoverflow.com/a/518232/2809427
+
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+# Read a file and split into lines
+
+
+def readLines(filename):
+    lines = open(filename).read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+
+# Build the category_lines dictionary, a list of lines per category
+category_lines = {}
+all_categories = []
+for filename in findFiles('data/names/*.txt'):
+    category = filename.split('/')[-1].split('.')[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+# Find letter index from all_letters, e.g. "a" = 0
+
+
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+
+
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+
+rnn = torch.load('results/char-classification.pt')
+rnn.eval()
+
+# Just return an output given a line
+
+
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+
+def predict(line, n_predictions=3):
+    output = evaluate(Variable(lineToTensor(line)))
+
+    # Get top N categories
+    topv, topi = output.data.topk(n_predictions, 1, True)
+    predictions = []
+
+    for i in range(n_predictions):
+        value = topv[0][i]
+        category_index = topi[0][i]
+        print('(%.2f) %s' % (value, all_categories[category_index]))
+        predictions.append([value, all_categories[category_index]])
+
+    return predictions
+
+
+@route('/<input_line>')
+def index(input_line):
+    print('line ', input_line)
+    result = predict(input_line, 1)
+    print(result)
+    return {'result': result[0][1]}
+
+
+run(host='localhost', port=5000)
